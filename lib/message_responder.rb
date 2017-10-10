dc5d50b28df3cf5c40463b17ff4c897800ae9c93
Dave Sherratt <dave.sherratt@ecrebo.com>
2017-10-10 11:14:36 +0100

    numbs

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -1,4 +1,6 @@
 require './models/user'
+require './models/planet'
+require './models/attack_target'
 require './models/ships'
 require './lib/message_sender'
 
@@ -97,163 +99,150 @@ puts @message.from
       end
     end
   end
-  def send_message(channel, text)
-        Slack.chat_postMessage(channel: channel, text: text)
-      end
 
-      def logger
-        @logger ||= begin
-          $stdout.sync = true
-          Logger.new(STDOUT)
-        end
-      end
+  def num(num)
+    num.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
 
-      def num(num)
-        num.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-      end
+  def rnd(num)
+    '%.2f' % num
+  end
 
-      def rnd(num)
-        '%.2f' % num
-      end
+  def ndate(date)
+    unless date.nil?
+      DateTime.strptime(date, '%Y-%m-%d %H:%M:%S%z').strftime("%d-%m-%Y")
+    end
+  end
 
-      def ndate(date)
-        unless date.nil?
-          DateTime.strptime(date, '%Y-%m-%d %H:%M:%S%z').strftime("%d-%m-%Y")
-        end
-      end
+  def number_short(num)
+    case num[-1].downcase
+      when "k"
+        num.gsub('k','000')
+      when "m"
+        num.gsub('m','000000')
+      when "b"
+        num.gsub('b','000000000')
+      else
+        num
+    end
+  end
 
-      def number_short(num)
-        case num[-1].downcase
-          when "k"
-            num.gsub('k','000')
-          when "m"
-            num.gsub('m','000000')
-          when "b"
-            num.gsub('b','000000000')
-          else
-            num
-        end
-      end
+  def number_nice(num)
+    num.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
+  end
 
-      def number_nice(num)
-        num.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
-      end
+  def efficiency_args?(check)
+    check =~ /(\d+(?:\.\d+)?[kmb]?)\s+(\w+)(?:\s+(t1|t2|t3))?/
+  end
 
-      def efficiency_args?(check)
-        check =~ /(\d+(?:\.\d+)?[kmb]?)\s+(\w+)(?:\s+(t1|t2|t3))?/
-      end
+  def letter?(check)
+    check =~ /\A[^A-Za-z$]+\Z/
+  end
 
-      def letter?(check)
-        check =~ /\A[^A-Za-z$]+\Z/
-      end
+  def number?(check)
+    check =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
+  end
 
-      def number?(check)
-        check =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/
-      end
+  def bravery(planet, target)
+    return [0.2,[2.2,target.score/planet.score].min-0.2].max * [0.2,[1.8,target.value/planet.value].min-0.1].max/((6+[4,planet.score/planet.value].max)/10)
+  end
 
-      def bravery(planet, target)
-        return [0.2,[2.2,target.score/planet.score].min-0.2].max * [0.2,[1.8,target.value/planet.value].min-0.1].max/((6+[4,planet.score/planet.value].max)/10)
-      end
+  def cap_rate(planet, target = nil)
+    paconfig = YAML.load(IO.read('config/pa.yml'))
+    mincap = paconfig['roids']['mincap']
+    maxcap = paconfig['roids']['maxcap']
+    if target == nil
+      return maxcap
+    end
+    mod = (planet.value/target.value)*0.5
+    return [mincap,[maxcap*modifier, maxcap].min].max
+  end
 
-      def cap_rate(planet, target = nil)
-        paconfig = YAML.load(IO.read('config/pa.yml'))
-        mincap = paconfig['roids']['mincap']
-        maxcap = paconfig['roids']['maxcap']
-        if target == nil
-          return maxcap
-        end
-        mod = (planet.value/target.value)*0.5
-        return [mincap,[maxcap*modifier, maxcap].min].max
-      end
+  def calc_xp(planet, target, cap = nil)
+    cap = max_cap(planet, target) if cap == nil
+    return cap * (bravery(planet, target) * 10)
+  end
 
-      def calc_xp(planet, target, cap = nil)
-        cap = max_cap(planet, target) if cap == nil
-        return cap * (bravery(planet, target) * 10)
-      end
+  def max_cap(planet, attacker = nil)
+    return (planet.size * cap_rate(planet, attacker))
+  end
 
-      def max_cap(planet, attacker = nil)
-        return (planet.size * cap_rate(planet, attacker))
-      end
+  def check_access(user, access)
+    user = User.where(:slack_id => user).first
+    if user.access >= access
+      return true
+    else
+      return false
+    end
+  end
 
-      def check_access(user, access)
-        user = User.where(:slack_id => user).first
-        if user.access >= access
-          return true
+  def addGalaxy(attack_id, x, y)
+    planets = Planet.where(:x => x).where(:y => y).where(:active => true)
+    if planets
+      planets.each do |planet|
+        target = AttackTarget.new(planet_id: planet.id, attack_id: attack_id)
+        if target.save
         else
-          return false
+          return "Error adding #{x}:#{y} "
         end
       end
+      return ""
+    else
+      return "#{x}:#{y} doesn't exist "
+    end
+  end
 
-      def addGalaxy(attack_id, x, y)
-        planets = Planet.where(:x => x).where(:y => y).where(:active => true)
-        if planets
-          planets.each do |planet|
-            target = AttackTarget.new(planet_id: planet.id, attack_id: attack_id)
-            if target.save
-            else
-              return "Error adding #{x}:#{y} "
-            end
-          end
-          return ""
+  def removeGalaxy(attack_id, x, y)
+    planets = Planet.where(:x => x).where(:y => y).where(:active => true)
+    if planets
+      planets.each do |planet|
+        target = AttackTarget.where(planet_id: planet.id, attack_id: attack_id).destroy_all
+        if target
         else
-          return "#{x}:#{y} doesn't exist "
+          return "Error removing #{x}:#{y} "
         end
       end
+      return ""
+    else
+      return "#{x}:#{y} doesn't exist "
+    end
+  end
 
-      def removeGalaxy(attack_id, x, y)
-        planets = Planet.where(:x => x).where(:y => y).where(:active => true)
-        if planets
-          planets.each do |planet|
-            target = AttackTarget.where(planet_id: planet.id, attack_id: attack_id).destroy_all
-            if target
-            else
-              return "Error removing #{x}:#{y} "
-            end
-          end
-          return ""
-        else
-          return "#{x}:#{y} doesn't exist "
-        end
+  def addPlanet(attack_id, x, y, z)
+    planet = Planet.where(:x => x).where(:y => y).where(:z => z).where(:active => true).first
+    if planet
+      target = AttackTarget.new(planet_id: planet.id, attack_id: attack_id)
+      if target.save
+        return ""
+      else
+        return "Error adding #{x}:#{y}:#{z} "
       end
+    else
+      return "#{x}:#{y}:#{z} doesn't exist "
+    end
+  end
 
-      def addPlanet(attack_id, x, y, z)
-        planet = Planet.where(:x => x).where(:y => y).where(:z => z).where(:active => true).first
-        if planet
-          target = AttackTarget.new(planet_id: planet.id, attack_id: attack_id)
-          if target.save
-            return ""
-          else
-            return "Error adding #{x}:#{y}:#{z} "
-          end
-        else
-          return "#{x}:#{y}:#{z} doesn't exist "
-        end
+  def removePlanet(attack_id, x, y, z)
+    planet = Planet.where(:x => x).where(:y => y).where(:z => z).where(:active => true).first
+    if planet
+      target = AttackTarget.where(planet_id: planet.id, attack_id: attack_id).destroy_all
+      if target
+        return ""
+      else
+        return "Error removing #{x}:#{y}:#{z} "
       end
+    else
+      return "#{x}:#{y}:#{z} doesn't exist "
+    end
+  end
 
-      def removePlanet(attack_id, x, y, z)
-        planet = Planet.where(:x => x).where(:y => y).where(:z => z).where(:active => true).first
-        if planet
-          target = AttackTarget.where(planet_id: planet.id, attack_id: attack_id).destroy_all
-          if target
-            return ""
-          else
-            return "Error removing #{x}:#{y}:#{z} "
-          end
-        else
-          return "#{x}:#{y}:#{z} doesn't exist "
-        end
-      end
-
-
-
-      def generateScanUrl(type,x,y,z,pa_scan_type)
-        paconfig = YAML.load(IO.read('config/pa.yml'))
-        if type == 'single'
-          url = paconfig['reqscan'].gsub('TTT', "#{pa_scan_type}").gsub('XXX', "#{x}").gsub('YYY', "#{y}").gsub('ZZZ', "#{z}")
-        end
-        return url
-      end
-
+  def generateScanUrl(type,x,y,z,pa_scan_type)
+    paconfig = YAML.load(IO.read('config/pa.yml'))
+    if type == 'single'
+      url = paconfig['reqscan'].gsub('TTT', "#{pa_scan_type}").gsub('XXX', "#{x}").gsub('YYY', "#{y}").gsub('ZZZ', "#{z}")
+    end
+    return url
+  end
 
   def answer_with_greeting_message
     answer_with_message I18n.t('greeting_message')
---
+++ models/attack_target.rb
@@ -0,0 +1,5 @@
+require 'active_record'
+
+class AttackTarget < ActiveRecord::Base
+	self.table_name = 'heresy_attack_target'
+end
