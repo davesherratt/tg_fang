8014f99f3081853a98f9154d1a6b44ac895ffea1
Dave Sherratt <dave.sherratt@ecrebo.com>
2017-10-14 19:43:55 +0100

    unit fix

--- models/unit_scan.rb
+++ models/unit_scan.rb
@@ -1,6 +1,6 @@
 require 'active_record'
 class UnitScan < ActiveRecord::Base
-  self.table_name = "fang_unitscan"
+  self.table_name = "heresy_unitscan"
   belongs_to :scan
   belongs_to :ships, :foreign_key => "ship_id"
 end
