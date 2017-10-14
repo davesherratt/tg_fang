4622440a264531b7be279d57b3e0407c96266746
Dave Sherratt <dave.sherratt@ecrebo.com>
2017-10-14 15:14:16 +0100

    markdown

--- lib/message_responder.rb
+++ lib/message_responder.rb
@@ -419,7 +419,7 @@ class MessageResponder
                 count += 1
                 res_message += "| #{count} | #{user.nick} | #{user.amps} |"
               end
-              bot.api.send_message chat_id: message.from.id, text: "#{res_message}", parse_mode: 'Markdown')
+              bot.api.send_message chat_id: message.from.id, text: "#{res_message}", parse_mode: "Markdown"
               bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "#{res_message}", parse_mode: 'Markdown')
             else
               bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "No scanners found with more than #{name} amps.")
@@ -437,7 +437,7 @@ class MessageResponder
                 count += 1
               res_message += "| #{count} | #{user.nick} | #{user.amps} |"
             end
-            bot.api.send_message chat_id: message.from.id, text: "#{res_message}", parse_mode: 'Markdown')
+            bot.api.send_message chat_id: message.from.id, text: "#{res_message}", parse_mode: "Markdown"
             bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "Top 10 amp scanners #{res_message}", parse_mode: 'Markdown')
           else
             bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "No scanners?")
@@ -654,7 +654,7 @@ class MessageResponder
       - myamps myplanet myphone news planet racism remuser req roidcost seagal search ship
       - sms smslog spam spamin stop top10 tick unit value whois xp
       ------------------"
-      bot.api.send_message chat_id: message.from.id, text: "#{msg}", parse_mode: "Markdown")
+      bot.api.send_message chat_id: message.from.id, text: "#{msg}", parse_mode: "Markdown"
       bot.api.send_message(chat_id: message.chat.id, reply_to_message_id: message.message_id, text: "#{msg}", parse_mode: 'Markdown')
     end
 
